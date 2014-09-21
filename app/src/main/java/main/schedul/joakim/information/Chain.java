dc64925c1808e85b08afc41e037e44ed939a8c81
Joakim Rishaug <Joakimrishaug@gmail.com>
2014-09-21 23:38:58 +0200

    Chain, Experience is roughly defined

---
+++ app/src/main/java/main/schedul/joakim/information/Chain.java
@@ -0,0 +1,80 @@
+package main.schedul.joakim.information;
+
+import android.text.format.Time;
+
+/**
+ * Created by NegatioN on 21.09.2014.
+ * The class represents a single user-defined chain that can generate experience based on doing the defined task.
+ */
+public class Chain {
+
+    private String name, description;
+    private long totalMins;
+    private Experience xp;
+    private int priority, currentChain;
+    private Time lastUpdated;
+
+    public Chain(String name, int priority, String description){
+        this.name = name;
+        this.priority = priority;
+        totalMins = 0;
+        xp = new Experience();
+        this.description = description;
+        currentChain = 0;
+    }
+
+    /*
+    when useer adds a task to current chain we calculate all stats
+     */
+    public void doTask(int minutes){
+        if(isChained())
+            currentChain++;
+        else
+            currentChain = 0;
+        xp.calculateExperience(minutes,currentChain);
+        totalMins+=minutes;
+    }
+
+    //is the task chained to it's last execution?
+    private boolean isChained(){
+        Time t = new Time();
+        t.setToNow();
+        //if more than 24 hours has passed since a user last chained a task, no bonus
+        if((t.toMillis(false) - lastUpdated.toMillis(false) > Time.HOUR * 24))
+            return false;
+        return true;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getTotalMins() {
+        return totalMins;
+    }
+
+    public void setTotalMins(long totalMins) {
+        this.totalMins = totalMins;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+}
---
+++ app/src/main/java/main/schedul/joakim/information/Experience.java
@@ -0,0 +1,35 @@
+package main.schedul.joakim.information;
+
+/**
+ * Created by NegatioN on 21.09.2014.
+ */
+public class Experience {
+    private long totalXp;
+
+    public Experience(){
+        totalXp = 0;
+    }
+
+    //takes in the user-defined minutes that the current task lasted.
+    //gets the current chain and calulates extra xp based on this too.
+    public void calculateExperience(int minutes, int chain){
+        double multiplier;
+        if(chain == 0)
+            multiplier = 1.2;
+        else{
+            multiplier = (1 + (chain * 0.1));
+        }
+
+        updateTotalXp(multiplier*minutes);
+    }
+
+
+    public long getTotalXp() {
+        return totalXp;
+    }
+
+    //simply adds xp to the total count
+    public void updateTotalXp(double taskXp) {
+        this.totalXp += (long) taskXp;
+    }
+}
