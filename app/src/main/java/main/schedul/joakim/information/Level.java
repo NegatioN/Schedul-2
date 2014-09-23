767270ce13cb9a5a14a890cd1ecb151ef4518b3e
Joakim Rishaug <Joakimrishaug@gmail.com>
2014-09-23 10:10:33 +0200

    Level-class rules for level-up

--- app/src/main/java/main/schedul/joakim/information/Level.java
+++ app/src/main/java/main/schedul/joakim/information/Level.java
@@ -6,10 +6,48 @@ package main.schedul.joakim.information;
 public class Level {
     //TODO skeleton, implement rules for leveling up lvl*qofficient = reqXP
     private int level;
+    private int levelXp;
+
+
+    //final static variables for computing the rule-set of experience
+    private static final double COEFFICIENT = 1.5d;
+    private static final int BASEEXPERIENCE = 100;
 
 
     public Level(){
         this.level = 0;
+        this.levelXp = 0;
+
+    }
+
+
+    public void passInExperience(int experience){
+        //add experience
+        levelXp+=experience;
+        if(isLevel())
+            //TODO some sort of animation for level-up
+        return;
+
+    }
+
+    //what experience is required to surpass the user's current level?
+    public int requiredXp(){
+       return (int) (level*COEFFICIENT*BASEEXPERIENCE);
+    }
+
+    //has the user leveled up?
+    private boolean isLevel(){
+        if(levelXp >= requiredXp()) {
+            levelUp();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    private void levelUp(){
+        level++;
+        levelXp = levelXp - requiredXp();
     }
 
 }
