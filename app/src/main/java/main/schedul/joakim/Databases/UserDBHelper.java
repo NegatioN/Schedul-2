e42e45458b06382b3d5ae485df0dd7f6d5cd2cc1
Joakim Rishaug <Joakimrishaug@gmail.com>
2014-11-04 23:39:58 +0100

    CRU-Functionality added for chains and achievements

--- app/src/main/java/main/schedul/joakim/Databases/UserDBHelper.java
+++ app/src/main/java/main/schedul/joakim/Databases/UserDBHelper.java
@@ -11,7 +11,11 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import main.schedul.joakim.information.Achievement;
 import main.schedul.joakim.information.Chain;
+import main.schedul.joakim.information.ComboAchievement;
+import main.schedul.joakim.information.ExperienceAchievement;
+import main.schedul.joakim.information.TimeAchievement;
 import main.schedul.joakim.information.User;
 
 /**
@@ -21,11 +25,20 @@ public class UserDBHelper extends SQLiteOpenHelper {
     private static final String NAME = "SchedulDB";
     private final static String TABLE_USERS = "Users";
     private final static String TABLE_CHAINS = "Chains";
+    private final static String TABLE_ACHIEVEMENTS = "Achievements";
     private final static int DB_VERSION = 2;
 
-    private final static String KEY_USERID = "_ID", KEY_USERNAME = "_NAME", KEY_LEVEL = "_LEVEL", KEY_LEVELXP = "_LEVELXP";
+    //Keys for user-table
+    private final static String KEY_USERID = "_USERID", KEY_USERNAME = "_NAME", KEY_LEVEL = "_LEVEL", KEY_LEVELXP = "_LEVELXP";
+    //Keys for Chain-table
     private final static String KEY_CHAINID = "_ID", KEY_CHAINNAME = "_CHAIN", KEY_CHAINDESC = "_DESCRIPTION", KEY_CHAINMINUTES = "_MINUTES",
             KEY_CHAINCOMBO = "_COMBO", KEY_MUSTCHAINSETTING = "_MUSTCHAIN", KEY_LASTUPDATE = "_LASTUPDATE", KEY_MINUTESTODAY = "_MINSTODAY", KEY_CHAINPRIORITY = "_PRIORITY";
+    //Keys for achievement-table
+    private final static String KEY_ACH_ID = "_ID", KEY_ACH_TYPE = "_TYPE", KEY_ACH_NAME = "_NAME", KEY_ACH_ACHIEVED = "_ACHIEVED", KEY_ACH_SIMPLE_OR_TOTAL = "_SIMPLEORTOTAL",
+    KEY_ACH_GOAL = "_GOAL";
+    //Achievement-types
+    private static final int TYPE_COMBO = 1, TYPE_EXPERIENCE = 2, TYPE_TIME = 3;
+    private static final int TYPE_SIMPLE = 4, TYPE_TOTAL = 5;
 
 
     private final static String DATEFORMAT = "yyyy-MM-dd-HH-mm-ss";
@@ -36,6 +49,7 @@ public class UserDBHelper extends SQLiteOpenHelper {
     }
 
 
+    //create our three main tables if they are not already present
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         String createTable = "CREATE TABLE " + TABLE_USERS + "(" +
@@ -59,6 +73,16 @@ public class UserDBHelper extends SQLiteOpenHelper {
 
         sqLiteDatabase.execSQL(createTable);
 
+        createTable = "CREATE TABLE " + TABLE_ACHIEVEMENTS + "(" +
+                KEY_USERID + " INTEGER FOREIGN KEY, " +
+                KEY_ACH_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                KEY_ACH_TYPE + " INTEGER NOT NULL, " +
+                KEY_ACH_NAME + " TEXT, " +
+                KEY_ACH_ACHIEVED + " INTEGER NOT NULL, " +
+                KEY_ACH_SIMPLE_OR_TOTAL + " INTEGER, " +
+                KEY_ACH_GOAL + " INTEGER)";
+
+        sqLiteDatabase.execSQL(createTable);
     }
 
     @Override
@@ -74,7 +98,6 @@ public class UserDBHelper extends SQLiteOpenHelper {
 
         ContentValues values = new ContentValues();
         values.put(KEY_USERNAME, user.getName());
-        //TODO create method for adding a list of chains in a different table, with userId as FK
         //TODO create method for adding a list of Achievements in a different table, with userId as FK
         values.put(KEY_LEVELXP, user.getLevel().getLevelXp());
         values.put(KEY_LEVEL, user.getLevel().getLevel());
@@ -83,26 +106,25 @@ public class UserDBHelper extends SQLiteOpenHelper {
 
         db.insert(TABLE_USERS, null, values);
         db.close();
+
+        //Add all chains to the user.
+        addChainList(getLastInsertedUserId() ,user.getUserChains());
     }
 
 
 
     //METHODS FOR INTEREACTING WITH CHAIN-DATABASE
 
+    //TODO effectivize by making method with only one db-access?
+    public void addChainList(int userid, List<Chain> chains){
+        for(Chain chain : chains)
+            addChain(userid,chain);
+    }
+
     public void addChain(int userId, Chain chain){
         SQLiteDatabase db = this.getWritableDatabase();
 
-        ContentValues values = new ContentValues();
-        values.put(KEY_USERID, userId);
-        values.put(KEY_CHAINNAME, chain.getName());
-        values.put(KEY_CHAINDESC, chain.getDescription());
-        values.put(KEY_CHAINMINUTES, chain.getTotalMins());
-        values.put(KEY_CHAINCOMBO, chain.getCurrentChain());
-        values.put(KEY_MINUTESTODAY, chain.getMinutesSpentToday());
-        values.put(KEY_LASTUPDATE, makeTimeToString(chain.getLastUpdated()));
-        values.put(KEY_CHAINPRIORITY, chain.getPriority());
-
-        db.insert(TABLE_CHAINS, null, values);
+        db.insert(TABLE_CHAINS, null, createChainValues(chain));
         db.close();
 
     }
@@ -137,6 +159,105 @@ public class UserDBHelper extends SQLiteOpenHelper {
         return chains;
     }
 
+    //update a single chain
+    public int updateChain(Chain chain){
+        SQLiteDatabase db = getWritableDatabase();
+
+        return db.update(TABLE_CHAINS, createChainValues(chain), KEY_CHAINID + "=?", new String[]{String.valueOf(chain.getChainid())});
+    }
+
+    //help method for creating chain content values.
+    private ContentValues createChainValues(Chain chain){
+        ContentValues values = new ContentValues();
+        values.put(KEY_CHAINNAME, chain.getName());
+        values.put(KEY_CHAINDESC, chain.getDescription());
+        values.put(KEY_CHAINMINUTES, chain.getTotalMins());
+        values.put(KEY_CHAINCOMBO, chain.getCurrentChain());
+        values.put(KEY_MINUTESTODAY, chain.getMinutesSpentToday());
+        values.put(KEY_LASTUPDATE, makeTimeToString(chain.getLastUpdated()));
+        values.put(KEY_CHAINPRIORITY, chain.getPriority());
+        values.put(KEY_MUSTCHAINSETTING, chain.getMustChainDays());
+        return values;
+    }
+
+    //METHODS TO INTERACT WITH ACHIEVEMENT-DB
+
+    public void addAchievement(Achievement achievement){
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.insert(TABLE_ACHIEVEMENTS, null, createAchievementValues(achievement));
+        db.close();
+
+    }
+
+    public int updateAchievement(Achievement achievement){
+        SQLiteDatabase db = getWritableDatabase();
+        return db.update(TABLE_ACHIEVEMENTS, createAchievementValues(achievement), KEY_ACH_ID + "=?", new String[]{String.valueOf(achievement.getId())});
+    }
+
+    //returns all achievements for a given user
+    public List<Achievement> getAchievements(int userId){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query(TABLE_ACHIEVEMENTS, new String[]{KEY_ACH_ID, KEY_ACH_ACHIEVED, KEY_ACH_GOAL, KEY_ACH_NAME, KEY_ACH_TYPE, KEY_ACH_SIMPLE_OR_TOTAL}, KEY_USERID + "=?",
+                new String[]{String.valueOf(userId)}, null, null, null, null);
+
+        List<Achievement> achievements = new ArrayList<Achievement>();
+        if(cursor != null){
+            cursor.moveToFirst();
+            do{
+                int id = cursor.getInt(0);
+                boolean achieved = cursor.getInt(1)==1?true:false; //return true if value is 1, else false
+                int goalNumber = cursor.getInt(2);
+                String name = cursor.getString(3);
+                int type = cursor.getInt(4);
+                int simpleOrTotal = cursor.getInt(5);
+
+                switch (type){
+                    case TYPE_COMBO:
+                        achievements.add(new ComboAchievement(id, name, goalNumber, achieved));
+                        break;
+                    case TYPE_EXPERIENCE:
+                        achievements.add(new ExperienceAchievement(id, name, goalNumber, simpleOrTotal==TYPE_TOTAL ? true: false, achieved));
+                        break;
+                    default:
+                        achievements.add(new TimeAchievement(id, name, goalNumber, simpleOrTotal==TYPE_TOTAL ? true : false, achieved));
+                }
+
+            }while(cursor.moveToNext());
+        }
+
+        db.close();
+        return achievements;
+    }
+
+
+    //helper method for creating content-values for the achievement
+    private ContentValues createAchievementValues(Achievement achievement){
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_ACH_ACHIEVED, achievement.isAchieved()?1:0); //use 1 or 0 as boolean values
+        values.put(KEY_ACH_GOAL, achievement.getGoalNumber());
+        values.put(KEY_ACH_NAME, achievement.getName());
+
+
+        //sets the achievement-type
+        if(achievement instanceof ComboAchievement) {
+            values.put(KEY_ACH_TYPE, TYPE_COMBO);
+        }
+        else if(achievement instanceof ExperienceAchievement) {
+            values.put(KEY_ACH_TYPE, TYPE_EXPERIENCE);
+            boolean totalGoal = ((ExperienceAchievement) achievement).isTotalGoal();
+            values.put(KEY_ACH_SIMPLE_OR_TOTAL, totalGoal ? TYPE_TOTAL : TYPE_SIMPLE);
+
+        }
+        else if(achievement instanceof TimeAchievement) {
+            boolean totalGoal = ((TimeAchievement) achievement).isTotalGoal();
+            values.put(KEY_ACH_SIMPLE_OR_TOTAL, totalGoal ? TYPE_TOTAL : TYPE_SIMPLE);
+            values.put(KEY_ACH_TYPE, TYPE_TIME);
+        }
+        return values;
+    }
+
     //DATE HELPER METHODS
 
     private String makeTimeToString(Time time){
@@ -157,6 +278,19 @@ public class UserDBHelper extends SQLiteOpenHelper {
 
         t.set(seconds, minutes,hour,day,month,year);
         return t;
+    }
+
+    //Helper method for getting the last user inserted into db.
+    private int getLastInsertedUserId(){
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query(TABLE_USERS, new String[]{KEY_USERID},null, null, null, null, null);
+
+        if(cursor != null) {
+            cursor.moveToLast();
+            return cursor.getInt(0);
+        }
+
+        return -1;
     }
 
 
